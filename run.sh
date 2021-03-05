682cc8c03f4fd265363bf36b52acf1fcbfd07750
John Evans <john.evans@digital.justice.gov.uk>
2021-03-05 14:18:55 +0000

    🛠 Get user and duration from env vars in run script

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ OUTPUT_PATH=cpmg-gatling-performance-tests
 
 aws s3 cp s3://${S3_BUCKET_NAME}/${OUTPUT_PATH}/defendantMatch.csv /app/src/test/resources/mined-data
 
-mvn gatling:test -DUsers=1 -DDuration=1 -DEnv=preprod -DData=offenderData -q
+mvn gatling:test -DUsers=${PERF_TEST_USERS} -DDuration=${PERF_TEST_DURATION} -DEnv=preprod -DData=offenderData -q
 
 aws s3 cp /app/target/gatling/ s3://${S3_BUCKET_NAME}/${OUTPUT_PATH}/${TIMESTAMP}/reports --recursive
 aws s3 cp /app/createOutputVariables.csv s3://${S3_BUCKET_NAME}/${OUTPUT_PATH}/${TIMESTAMP}/createOutputVariables.csv
