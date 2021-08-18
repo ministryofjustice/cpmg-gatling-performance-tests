fd1e0b9dea0d60abed9fcede163d58aeab087bea
John Evans <john.evans@digital.justice.gov.uk>
2021-08-18 15:07:13 +0100

    🚛  Change output directory structure to make navigation easier (#20)
    * 🚛  Change output directory structure to make navigation easier
    
    * 🐛  Fix missing variable

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
 TIMESTAMP=$(date +"%Y-%m-%dT%H-%M-%S")
+DATE=$(date +"%Y-%m-%d")
+TIME=$(date +"%H-%M-%S")
 OUTPUT_PATH=cpmg-gatling-performance-tests
 
 aws s3 cp s3://${S3_BUCKET_NAME}/${OUTPUT_PATH}/defendantMatch.csv /app/src/test/resources/mined-data
 
 mvn gatling:test -DUsers=${PERF_TEST_USERS} -DDuration=${PERF_TEST_DURATION} -DEnv=preprod -DData=offenderData -q
 
-aws s3 cp /app/target/gatling/ s3://${S3_BUCKET_NAME}/${OUTPUT_PATH}/${TIMESTAMP}/reports --recursive
+aws s3 cp /app/target/gatling/ s3://${S3_BUCKET_NAME}/${OUTPUT_PATH}/${DATE}/${TIME}/reports --recursive
 aws s3 cp /app/createOutputVariables.csv s3://${S3_BUCKET_NAME}/${OUTPUT_PATH}/${TIMESTAMP}/createOutputVariables.csv
